c061e93f08ae060b01ffcbb29686c0a15abbc532
jmjp775 <jmjp775@gmail.com>
2021-05-17 13:36:38 -0400

    added stashed changes

---
+++ LibraryProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientListForm.Entities;
+
+namespace ClientListForm
+{
+    /// <summary>
+    /// LibraryProvider is the base class for the method GetLibraryData
+    /// </summary>
+    public abstract class LibraryProvider
+    {
+        public abstract Library GetLibraryData();
+    }
+}
